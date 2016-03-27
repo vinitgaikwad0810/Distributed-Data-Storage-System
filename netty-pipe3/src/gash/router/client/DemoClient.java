23b42ded40d241631d3e6755ceaad4fc8be68e85
jagrutipatil <jagrutipatil32@gmail.com>
2016-03-27 02:41:15 -0700

    Demo Client

--- netty-pipe3/src/gash/router/client/DemoClient.java
+++ netty-pipe3/src/gash/router/client/DemoClient.java
@@ -1,36 +1,14 @@
 package gash.router.client;
 
-import java.io.IOException;
+import gash.router.client.api.ImageClient;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-
-public class QueueClient {
-	
-	public void createQueue() throws IOException {
-		ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("localhost");
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
-	    
-	    channel.queueDeclare("inbound_queue", false, false, false, null);
-	    String message = "Hello World!";
-	    channel.basicPublish("", "inbound_queue", null, message.getBytes());
-	    System.out.println(" [x] Sent '" + message + "'");
-	    
-	    channel.close();
-	    connection.close();
-	}
-	
-	
+public class DemoClient {	
 	
 	public static void main(String args[]) {		
-		try {
-			QueueClient client = new QueueClient();
-			client.createQueue();			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	
+		 ImageClient client = new ImageClient();
+		 
+		 String key = client.post("/home/jagruti/Pictures/2_1_after_Assigning_IP.png");
+		 client.get(key, "/home/jagruti/workspace/ProjectFluffy/ImageTransfer/src");
+		
 	}
 }
