8b56577149730cdb87044fa5a0be7e12cd3b9d77
vinitgaikwad0810 <vinit.gaikwad10@gmail.com>
2016-04-08 17:39:04 -0700

    New Protoc Classes

--- RAFT/src/deven/monitor/client/MonitorClientApp.java
+++ RAFT/src/deven/monitor/client/MonitorClientApp.java
@@ -19,9 +19,10 @@ import raft.NodeState;
 import raft.proto.Monitor.ClusterMonitor;
 import raft.proto.Work.WorkMessage;
 
-public class MonitorClientApp implements MonitorListener{
+public class MonitorClientApp implements MonitorListener {
 	private MonitorClient mc;
-	
+	private static int ticker = 0;
+
 	public MonitorClientApp(MonitorClient mc) {
 		init(mc);
 	}
@@ -30,43 +31,45 @@ public class MonitorClientApp implements MonitorListener{
 		this.mc = mc;
 		this.mc.addListener(this);
 	}
-	
-	public ClusterMonitor sendDummyMessage(int noOfNodes,int addProcessedId) {
+
+	public ClusterMonitor sendDummyMessage(int noOfNodes, int addProcessedId) {
 		/*
-		 * This message should be created and sent by only one node inside the cluster.
-		  	
-		  */
+		 * This message should be created and sent by only one node inside the
+		 * cluster.
+		 * 
+		 */
 		try {
-		
-		//Build the message to be sent to monitor server
-		ClusterMonitor.Builder cm = ClusterMonitor.newBuilder();
-		//your cluster ID
-		cm.setClusterId(989);
-		//No of nodes in your cluster
-		cm.setNumNodes(noOfNodes);
-		//Node Id = Process Id
-		cm.addProcessId(0);
-		cm.addProcessId(1);
-		//Set processId,No of EnquedTask for that processId
-		cm.addEnqueued(5);
-		//cm.addEnqueued(5);
-		//Set processId,No of ProcessedTask for that processId
-		cm.addProcessed(addProcessedId);
-//		cm.addProcessed(3);
-		//Set processId,No of StolenTask for that processId
-		cm.addStolen(2);
-		cm.addStolen(2);
-		//Increment tick every time you send the message, or else it would be ignored
-		// Tick starts from 0
-		cm.setTick(0);
-		
-		return cm.build();
-		}catch(Exception e) {
+
+			// Build the message to be sent to monitor server
+			ClusterMonitor.Builder cm = ClusterMonitor.newBuilder();
+			// your cluster ID
+			cm.setClusterId(989);
+			// No of nodes in your cluster
+			cm.setNumNodes(noOfNodes);
+			// Node Id = Process Id
+			cm.addProcessId(0);
+			cm.addProcessId(1);
+			// Set processId,No of EnquedTask for that processId
+			cm.addEnqueued(5);
+			// cm.addEnqueued(5);
+			// Set processId,No of ProcessedTask for that processId
+			cm.addProcessed(addProcessedId);
+			// cm.addProcessed(3);
+			// Set processId,No of StolenTask for that processId
+			cm.addStolen(2);
+			cm.addStolen(2);
+			// Increment tick every time you send the message, or else it would
+			// be ignored
+			// Tick starts from 0
+			cm.setTick(ticker++);
+
+			return cm.build();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	@Override
 	public String getListenerID() {
 		// TODO Auto-generated method stub
@@ -78,33 +81,25 @@ public class MonitorClientApp implements MonitorListener{
 		// TODO Auto-generated method stub
 	}
 	/*
-	public static void main(String[] args) {
-		
-		String host = "127.0.0.1";
-		int port = 5000;
-
-		try {
-			MonitorClient mc = new MonitorClient(host, port);
-			MonitorClientApp ma = new MonitorClientApp(mc);
-
-			
-			
-			// do stuff w/ the connection
-			System.out.println("Creating message");
-			ClusterMonitor msg = ma.sendDummyMessage();
-			System.out.println("Sending generated message");
-			mc.write(msg);
-			
-			System.out.println("\n** exiting in 10 seconds. **");
-			System.out.flush();
-			Thread.sleep(10 * 1000);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			
-		}
-	}
-
-	
-*/
+	 * public static void main(String[] args) {
+	 * 
+	 * String host = "127.0.0.1"; int port = 5000;
+	 * 
+	 * try { MonitorClient mc = new MonitorClient(host, port); MonitorClientApp
+	 * ma = new MonitorClientApp(mc);
+	 * 
+	 * 
+	 * 
+	 * // do stuff w/ the connection System.out.println("Creating message");
+	 * ClusterMonitor msg = ma.sendDummyMessage(); System.out.println(
+	 * "Sending generated message"); mc.write(msg);
+	 * 
+	 * System.out.println("\n** exiting in 10 seconds. **"); System.out.flush();
+	 * Thread.sleep(10 * 1000); } catch (Exception e) { e.printStackTrace(); }
+	 * finally {
+	 * 
+	 * } }
+	 * 
+	 * 
+	 */
 }
