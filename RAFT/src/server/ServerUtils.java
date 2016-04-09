f9bcfac5b35d79452115a1e17d536382a9ae2430
Faisal <m.faisal.92.27@gmail.com>
2016-04-08 21:52:33 -0700

    Random Timer Fixed

--- RAFT/src/server/ServerUtils.java
+++ RAFT/src/server/ServerUtils.java
@@ -12,7 +12,7 @@ public class ServerUtils {
 
 	public static long getElectionTimeout() {
 		Random random = new Random();
-		long variableTimeout = random.nextInt((int) ((10000L - 1000L) + 1L)) + 6000L;
+		long variableTimeout = random.nextInt((int) ((200000L - 1000L) + 1L));
 		long currentTimeout = 30000L + (long) variableTimeout;
 		return currentTimeout;
 
